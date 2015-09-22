6f40fea650fb861de59efe029116b2e18e372d27
Fernando Matarrubia <f.matarrubiagarcia@gmail.com>
2015-09-21 22:51:30 -0700

    Adding Tests 9. and 10. and fixing some broken tests

--- Udacity_P2/tournament_test.py
+++ Udacity_P2/tournament_test.py
@@ -67,10 +67,9 @@ def testStandingsBeforeMatches():
                          "they have played any matches.")
     elif len(standings) > 2:
         raise ValueError("Only registered players should appear in standings.")
-    if len(standings[0]) != 4:
-        raise ValueError("Each playerStandings row should have four columns.")
-    [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)] = standings
-    print standings
+    if len(standings[0]) != 5:
+        raise ValueError("Each playerStandings row should have five columns.")
+    [(id1, name1, tournament1, wins1, matches1), (id2, name2, tournament2, wins2, matches2)] = standings
     if standings[0]['matches'] != 0 or standings[1]['matches'] != 0 or standings[0]['wins'] != 0 or standings[1]['wins'] != 0:
         raise ValueError(
             "Newly registered players should have no matches or wins.")
@@ -91,7 +90,8 @@ def testReportMatches():
     [id1, id2, id3, id4] = [row['id'] for row in standings]
     reportMatch(1, id1, id2, 'testReportMatches1')
     reportMatch(1, id3, id4, 'testReportMatches2')
-    standings = playerStandings()
+    # The matches are created in tournament = 1, so we use that one for retrieving the new standings
+    standings = playerStandingsInTournament(1)
     for row in standings:
         i = row["id"];
         m = row["matches"];
@@ -116,17 +116,61 @@ def testPairings():
     [id1, id2, id3, id4] = [row['id'] for row in standings]
     reportMatch(1, id1, id2, 'testReportMatches1')
     reportMatch(1, id3, id4, 'testReportMatches2')
-    pairings = swissPairings()
+    # The matches are created in tournament = 1, so we use that one for the swissPairing
+    pairings = swissPairings(1)
     if len(pairings) != 2:
         raise ValueError(
             "For four players, swissPairings should return two pairs.")
     [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4)] = pairings
     correct_pairs = set([frozenset([id1, id3]), frozenset([id2, id4])])
-    actual_pairs = set([frozenset([pid1, pid2]), frozenset([pid3, pid4])])
+    actual_pairs = set([frozenset([pairings[0]["id1"], pairings[0]["id2"]]), frozenset([pairings[1]["id1"], pairings[1]["id2"]])])
     if correct_pairs != actual_pairs:
         raise ValueError(
             "After one match, players with one win should be paired.")
     print "8. After one match, players with one win are paired."
+
+def testRematch():
+    deleteMatches()
+    deletePlayers()
+    registerPlayer("Twilight Sparkle")
+    registerPlayer("Fluttershy")
+    standings = playerStandings()
+    [id1, id2] = [row['id'] for row in standings]
+    reportMatch(1, id1, id2, 'testRematch1')
+    try:
+        # The database unique index should prevent this rematch
+        reportMatch(1, id2, id1, 'testRematch2')
+    except:
+        print "9. Players weren't allowed to play a rematch."
+        return
+
+    raise ValueError(
+            "Two players shouldn't be able to rematch.")
+
+
+def testTournament():
+    deleteMatches()
+    deletePlayers()
+    registerPlayer("Twilight Sparkle")
+    registerPlayer("Fluttershy")
+    registerPlayer("Applejack")
+    registerPlayer("Pinkie Pie")
+    standings = playerStandings()
+    [id1, id2, id3, id4] = [row['id'] for row in standings]
+    reportMatch(1, id1, id2, 'testTournament1-1')
+    reportMatch(1, id3, id4, 'testTournament1-2')
+    reportMatch(2, id1, id2, 'testTournament2-1')
+    reportMatch(2, id3, id4, 'testTournament2-2')
+    standings = playerStandingsInTournament(1)
+    if len(standings) != 4:
+        raise ValueError(
+            "There should be a total of four standings in the first tournament.")
+    standings = playerStandingsInTournament(2)
+    if len(standings) != 4:
+        raise ValueError(
+            "There should be a total of four standings in the second tournament.")
+    
+    print "10. After creating two pairs of matches as part of two different tournaments, no errors raised."
 
 
 if __name__ == '__main__':
@@ -138,6 +182,8 @@ if __name__ == '__main__':
     testStandingsBeforeMatches()
     testReportMatches()
     testPairings()
+    testRematch()
+    testTournament()
     print "Success!  All tests pass!"
 
 
