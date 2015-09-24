090597c22bbba1e362ef226d803e0713018306a7
Fernando Matarrubia <f.matarrubiagarcia@gmail.com>
2015-09-23 23:25:33 -0700

    Dividing sql file in two pieces

--- Udacity_P2/tournament.sql
+++ Udacity_P2/tournament.sql
@@ -47,21 +47,5 @@ CREATE VIEW playerStandings AS
 	ORDER BY wins DESC;
 
 
--- Some test insertions here, so we can avoid doing everything manually
-INSERT INTO players(name) VALUES ('Michael Jordan'); 	-- id: 1
-INSERT INTO players(name) VALUES ('Shaquille ONeal'); 	-- id: 2
-INSERT INTO players(name) VALUES ('Carmelo Anthony'); 	-- id: 3
-INSERT INTO players(name) VALUES ('Stephen Curry');		-- id: 4
-INSERT INTO players(name) VALUES ('Kevin Durant');		-- id: 5
-INSERT INTO players(name) VALUES ('Pau Gasol');			-- id: 6
-
-
-INSERT INTO tournaments(name) VALUES ('NPPA Season 2014/2015'); 	-- id: 1
-INSERT INTO tournaments(name) VALUES ('NPPA Season 2015/2016'); 	-- id: 2
-
-INSERT INTO matches VALUES (1, 1, 2, 'Bulls vs Lakers');
-INSERT INTO matches VALUES (1, 3, 4, 'Knicks vs Warriors');
-INSERT INTO matches VALUES (1, 4, 2, 'Warriors vs Lakers');
-INSERT INTO matches VALUES (2, 4, 2, 'Warriors vs Knicks');
 
 
