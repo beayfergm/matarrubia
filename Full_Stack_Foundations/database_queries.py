678745d14d3a85823b73183722c25c43a07da2ed
Fernando Matarrubia <beayfergm@gmail.com>
2015-09-27 17:54:51 -0700

    Updating queries with updates and deletes

--- Full_Stack_Foundations/database_queries.py
+++ Full_Stack_Foundations/database_queries.py
@@ -10,8 +10,26 @@ DBSession = sessionmaker(bind = engine)
 
 session = DBSession()
 
-items = session.query(MenuItem).all()
+# Shows all the Veggie Burgers
+items = session.query(MenuItem).filter_by(name = "Veggie Burger")
+itemnames = [[item.id, item.restaurant.name, item.price] for item in items]
+print 'There are {0} Menu Items with the name Veggie Burger'.format(len(itemnames))
 
-itemnames = [item.name for item in items]
+# Updates a unique instance of the Veggie Burger
+menuItemToUpdate = session.query(MenuItem).filter_by(id = 12).one()
+menuItemToUpdate.price = '$2.99'
+session.add(menuItemToUpdate)
+session.commit()
+print menuItemToUpdate.restaurant.name +"'s "+ menuItemToUpdate.name + "'s price has been updated"
 
-print itemNames
+# Updates all the Veggie Burgers
+for item in items:
+	item.price = '$2.99'
+	session.add(item)
+	session.commit()
+print "All Veggie Burger prices have been updated to $2.99"
+
+# Deletes a single item
+menuItemToDelete = session.query(MenuItem).filter_by(name = 'Spinach Ice Cream').one()
+session.delete(menuItemToDelete)
+session.commit()
