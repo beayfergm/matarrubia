53136ddaf0f395de4344abfdf03dd0041aa454f1
Fernando Matarrubia <beayfergm@gmail.com>
2015-11-11 18:04:05 -0800

    Updating Project.py and adding render_template use

--- Full_Stack_Foundations/restaurant/project.py
+++ Full_Stack_Foundations/restaurant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -18,42 +18,30 @@ def helloWorld():
 
 @app.route("/restaurants/<int:restaurant_id>/")
 def restaurantMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
 	items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
 
 	if (items.count() == 0):
 		return "Restaurant id {0} not found".format(restaurant_id)
 		return
 
-	output = ""
-	for i in items:
-		print "item!"
-		output += ReturnItemDescription(i)
-		print output
+	output = RenderResponse(restaurant, items)
 	return output
 
 @app.route("/restaurants/<int:restaurant_id>/new/")
 def newMenuItem(restaurant_id):
 	return "Page to create a new menu item. Restaurant: {0}. Task 1 complete!".format(restaurant_id)
 
-@app.route("/restaurants/<int:restaurant_id>/edit/")
-def editMenuItem(restaurant_id):
-	return "Page to edit a menu item. Restaurant: {0}. Task 2 complete!".format(restaurant_id)
+@app.route("/restaurants/<int:restaurant_id>/<int:menu_id>/edit/")
+def editMenuItem(restaurant_id, menu_id):
+	return "Page to edit a menu item. Restaurant: {0}. Menu Item: {1}. Task 2 complete!".format(restaurant_id, menu_id)
 
 @app.route("/restaurants/<int:restaurant_id>/<int:menu_id>/delete/")
 def deleteMenuItem(restaurant_id, menu_id):
 	return "Page to delete a menu item. Restaurant: {0}. Menu Item: {1}. Task 3 complete!".format(restaurant_id, menu_id)
 
-def ReturnItemDescription(item):
-	output = ''
-	output += item.name
-	output += "</br>"
-	output += item.price
-	output += "</br>"
-	output += item.description
-	output += "</br>"
-	output += "</br>"
-
-	return output
+def RenderResponse(restaurant, items):
+	return render_template('menu.html', restaurant = restaurant, items = items)
 
 if __name__ == '__main__':
 	app.debug = True
